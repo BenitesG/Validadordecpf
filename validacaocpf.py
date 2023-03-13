939b30a148e94cfa93d4f642de4f63c626204ef0
Gustavo Oliveira Benites <95330379+BenitesG@users.noreply.github.com>
2023-03-13 16:28:56 -0300

    Verificador de CPF completo
    Adicionei a verificação do ultimo digito do cpf

--- validacaocpf.py
+++ validacaocpf.py
@@ -1,23 +1,47 @@
+import os
+
 while True:
     cpf_digitado = input('Digite o seu CPF sem caracteres especiais:')
     cpf_lista = []
-    i = 10
-    for numero_multi in cpf_digitado:
+    cpf_lista2 = []
+    contador_regressivo1 = 10
+    contador_regressivo2 = 11
+    for digito1_multi in cpf_digitado:
 
-        if numero_multi.isdigit():
-            numero_int = int(numero_multi)
-            cpf_lista.append(numero_int * i)
-            i -= 1
+        if digito1_multi.isdigit():
+            digito1_int = int(digito1_multi)
+            cpf_lista.append(digito1_int * contador_regressivo1)
+            contador_regressivo1 -= 1
 
-        if i == 1:
-            cpf_soma = (cpf_lista[0] + cpf_lista[1] + cpf_lista[2] + cpf_lista[3] +
-                        cpf_lista[4] + cpf_lista[5] + cpf_lista[6] +
-                        cpf_lista[7] + cpf_lista[8]) * 10 % 11
+        if contador_regressivo1 == 1:
+            soma_digito1 = (cpf_lista[0] + cpf_lista[1] + cpf_lista[2] + cpf_lista[3] +
+                            cpf_lista[4] + cpf_lista[5] + cpf_lista[6] +
+                            cpf_lista[7] + cpf_lista[8]) * 10 % 11
+
+    for digito2_multi in cpf_digitado:
+
+        if digito2_multi.isdigit():
+            digito2_int = int(digito2_multi)
+            cpf_lista2.append(digito2_int * contador_regressivo2)
+            contador_regressivo2 -= 1
+
+        if contador_regressivo2 == 0:
+            soma_digito2 = (cpf_lista2[0] + cpf_lista2[1] + cpf_lista2[2] + cpf_lista2[3] +
+                            cpf_lista2[4] + cpf_lista2[5] + cpf_lista2[6] +
+                            cpf_lista2[7] + cpf_lista2[8] + cpf_lista2[9]) * 10 % 11
 
     try:
-        if cpf_soma == cpf_lista[9]:
+        if soma_digito1 <= 9 and soma_digito2 <= 9:
+            if soma_digito1 == cpf_lista[9] and soma_digito2 == cpf_lista2[10]:
+                print('Cpf válido')
+                break
+            else:
+                os.system('cls')
+                print('Cpf inválido ou inexistente')
+        else:
+            soma_digito1 == 0 and soma_digito2 == 0
             print('Cpf válido')
             break
-
     except:
-        print('Cpf invalido, tente novamente')
+        os.system('cls')
+        print('Cpf inválido ou inexistente')
