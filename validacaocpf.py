116bac6fa5f902207aa824726d6fe9190f9fa93a
Gustavo Oliveira Benites <95330379+BenitesG@users.noreply.github.com>
2023-03-16 22:53:49 -0300

    Update validacaocpf.py

--- validacaocpf.py
+++ validacaocpf.py
@@ -1,12 +1,15 @@
 import os
 
 while True:
-    cpf_digitado = input('Digite o seu CPF sem caracteres especiais:')
+    cpf_digitado = input('Digite o seu CPF: ')
+    cpf_sem_caracter = cpf_digitado.replace('.', '') \
+        .replace('-', '')
     cpf_lista = []
     cpf_lista2 = []
     contador_regressivo1 = 10
     contador_regressivo2 = 11
-    for digito1_multi in cpf_digitado:
+
+    for digito1_multi in cpf_sem_caracter:
 
         if digito1_multi.isdigit():
             digito1_int = int(digito1_multi)
@@ -18,7 +21,7 @@ while True:
                             cpf_lista[4] + cpf_lista[5] + cpf_lista[6] +
                             cpf_lista[7] + cpf_lista[8]) * 10 % 11
 
-    for digito2_multi in cpf_digitado:
+    for digito2_multi in cpf_sem_caracter:
 
         if digito2_multi.isdigit():
             digito2_int = int(digito2_multi)
@@ -31,9 +34,10 @@ while True:
                             cpf_lista2[7] + cpf_lista2[8] + cpf_lista2[9]) * 10 % 11
 
     try:
+
         if soma_digito1 <= 9 and soma_digito2 <= 9:
             if soma_digito1 == cpf_lista[9] and soma_digito2 == cpf_lista2[10]:
-                print('Cpf válido')
+                print(f'Cpf: {cpf_digitado} é válido')
                 break
             else:
                 os.system('cls')
@@ -42,7 +46,7 @@ while True:
         elif soma_digito1 >= 10:
             soma_digito1 = 0
             if soma_digito1 == cpf_lista[9] and soma_digito2 == cpf_lista2[10]:
-                print('Cpf válido')
+                print(f'Cpf: {cpf_digitado} é válido')
                 break
             else:
                 os.system('cls')
@@ -51,7 +55,7 @@ while True:
         elif soma_digito2 >= 10:
             soma_digito2 = 0
             if soma_digito2 == cpf_lista2[10] and soma_digito1 == cpf_lista[9]:
-                print('Cpf válido')
+                print(f'Cpf: {cpf_digitado} é válido')
                 break
             else:
                 os.system('cls')
